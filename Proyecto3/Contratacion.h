b5ba1c8c48b272ec0e0d7078c37a0ded665249b8
jorgeramos2 <jorge.ramos1996@gmail.com>
2017-11-12 18:13:26 -0600

    ...

--- Proyecto3/Contratacion.h
+++ Proyecto3/Contratacion.h
@@ -2,7 +2,49 @@
 #define CONTRATACION_H_INCLUDED
 using namespace std;
 class Contratacion{
-
+public:
+///Constructores
+Contratacion();
+///Metodos de acceso
+string getClave();
+int getIdCliente();
+int getdiasDuracion();
+///Metodos de modificacion
+void setClave(string clave);
+void setIdCliente(int idCliente);
+void setdiasDUracion(int diasDuracion);
+private:
+///Atributos
+string clave;
+int idCliente;
+int diasDuracion;
 };
+///Constructores
 
+///Metodos de acceso
+string Contratacion::getClave()
+{
+    return clave;
+}
+int Contratacion::getIdCliente()
+{
+    return idCliente;
+}
+int Contratacion::getdiasDuracion()
+{
+    return diasDuracion;
+}
+///Metodos de modificacion
+void Contratacion::setClave(string clave)
+{
+    this->clave=clave;
+}
+void Contratacion::setIdCliente(int idCliente)
+{
+    this->idCliente=idCliente;
+}
+void Contratacion::setdiasDUracion(int diasDuracion)
+{
+    this->diasDuracion=diasDuracion;
+}
 #endif // CONTRATACION_H_INCLUDED
